2b30c57febadfdab362dcc9937416523e3d43a32
dinanjalee <chashikajw007@gmail.com>
2018-10-06 04:58:34 +0530

    create resource funtion template

--- SAMtoTerraform.py
+++ SAMtoTerraform.py
@@ -8,6 +8,26 @@ def createProvider(file,provider,region,access_key,secret_key):
     file.write("  secret_key = "+'"'+secret_key+'"\n')
     file.write("}\n"),
 
-createProvider(file,"aws","us-east-1","21212121","121213")
-file.close()
 
+
+
+def function_resource(file):
+    file.write("resource  "+ "aws_lambda_function " +'"example"' + " {\n"
+          " function_name = "+'"'+"ServerlessExample"+'"\n'
+            "\n"
+      
+          " s3_bucket = "+'"'+"terraform-serverless-example"+'"\n'
+          " s3_key    = "+'"'+"v1.0.0/example.zip"+'"\n'
+
+     
+          " handler = "+'"'+"main.handler"+'"\n'
+          " runtime = "+'"'+"nodejs6.10"+'"\n'
+
+          " role = "+"${aws_iam_role.lambda_exec.arn}\n"
+        "}"
+        )
+
+
+createProvider(file,"aws","us-east-1","21212121","121213")
+function_resource(file)
+file.close()
