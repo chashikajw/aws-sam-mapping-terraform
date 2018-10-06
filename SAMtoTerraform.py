56f5d84fc46c523fa3093c28d1d97ef782e066a4
dinanjalee <chashikajw007@gmail.com>
2018-10-06 06:56:27 +0530

    update model

--- SAMtoTerraform.py
+++ SAMtoTerraform.py
@@ -1,12 +1,9 @@
-
-file = open("terraformModel", "w")
-
 def createProvider(file,provider,region,access_key,secret_key):
-    file.write("provider "+'"'+provider+'"'+"{\n")
-    file.write("  region = "+'"'+region+'"\n')
-    file.write("  access_key = "+'"'+access_key+'"\n')
-    file.write("  secret_key = "+'"'+secret_key+'"\n')
-    file.write("}\n"),
+    file.write("provider "+'"'+provider+'"'+"{\n"
+            "  region = "+'"'+region+'"\n'
+            "  access_key = "+'"'+access_key+'"\n'
+            "  secret_key = "+'"'+secret_key+'"\n'
+            "}\n")
 
 
 
@@ -27,7 +24,22 @@ def function_resource(file):
         "}"
         )
 
+file = open("terraformModel", "w")
 
 createProvider(file,"aws","us-east-1","21212121","121213")
 function_resource(file)
 file.close()
+
+
+fh = open("hello_world.yaml.", "r")
+
+arr = []
+parmeters = {}
+for line in fh:
+    if(line!='\n'):
+        item = line.strip().split(":")
+        arr.append(item)
+        parmeters[item[0]] = [item[1]]
+        print (line)
+    
+    
